a928b6bf8f2d5ca1cd80dac415f7092076215f41
Jonas Cosandey <jonas.cosandey@redcor.ch>
2015-12-23 14:06:43 +0100

    added gui

--- src/Cell.java
+++ src/Cell.java
@@ -1,33 +1,88 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class Cell {
 	public final int cellID;
 	public final boolean bomb;
 	public boolean marked;
 	public boolean revealed;
+	public final JButton button = new JButton();
+	public int bombsInRange;
 	
-	public Cell(int cellID, boolean bomb) {
+	public Cell(int cellID, boolean bomb, JFrame layout) {
 		this.cellID = cellID;
 		this.bomb = bomb;
+		button.setFont(new Font("Arial", Font.PLAIN, 40));
+		button.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent mouseEvent) {
+		        int modifiers = mouseEvent.getModifiers();
+		        if ((modifiers & InputEvent.BUTTON1_MASK) == InputEvent.BUTTON1_MASK) {
+		        	reveal();
+		        }
+		        if ((modifiers & InputEvent.BUTTON3_MASK) == InputEvent.BUTTON3_MASK) {
+		        	mark();
+		        }
+			}
+		});
+		layout.add(button);  
 	}
 	
-	public boolean mark(boolean changeState) {
-		if (changeState) {
+	public void mark() {
+		if (!revealed) {
 			if (marked) {
 				marked = false;
+				button.setBackground(new JButton().getBackground());
 			} else {
 				marked = true;
+				button.setBackground(Color.RED);
 			}
-			return marked;
-		} else {
-			return marked;
 		}
 	}
 	
 	public void reveal() {
 		if (!revealed) {
 			revealed = true;
-		} else {
-			System.out.println("Bereits aufgedeckt");
+			button.setBackground(Color.WHITE);
+			button.setText(String.valueOf(bombsInRange));
+			setFontColor();
+		}
+	}
+	
+	private void setFontColor(){
+		switch (bombsInRange) {
+		case 0:
+			
+			break;
+		case 1:
+			button.setForeground(Color.RED);
+			break;
+		case 2:
+			button.setForeground(Color.RED);
+			break;
+		case 3:
+			button.setForeground(Color.RED);
+			break;
+		case 4:
+			button.setForeground(Color.RED);
+			break;
+		case 5:
+			button.setForeground(Color.RED);
+			break;
+		case 6:
+			button.setForeground(Color.RED);
+			break;
+		case 7:
+			button.setForeground(Color.RED);
+			break;
+		case 8:
+			button.setForeground(Color.RED);
+			break;
 		}
 	}
 }
--- src/Field.java
+++ src/Field.java
@@ -1,14 +1,38 @@
+import java.awt.GridLayout;
 import java.util.Random;
+
+import javax.swing.JFrame;
 
 public class Field {
 	private int[] bombs = new int[10];
 	private Cell[][] cells = new Cell[8][8];
+	public final JFrame frame = new JFrame();
 	
 	public Field() {
+		frame.setSize(500,500);
+		GridLayout layout = new GridLayout(8,8);
+		frame.setLayout(layout);;
+		
 		generateBombs(this.bombs);
-		generateCells(this.cells, this.bombs);
+		generateCells(this.cells, this.bombs, frame);
+		
+		frame.setResizable(false);
+		frame.setVisible(true);
+		
+		setBombInRange();
 	}
 	
+	private void setBombInRange(){
+		int[][] range = new int[8][2];
+		for (int x = 0; x < cells.length; x++) {
+			for (int y = 0; y < cells[x].length; y++) {
+				for (int i = 0; i < range.length; i++) {
+					
+				}
+			}
+		}
+	}
+		
 	private void generateBombs(int[] bombs){
 		Random random = new Random();
 		int randInt;
@@ -31,15 +55,15 @@ public class Field {
 		}
 	}
 	
-	private void generateCells(Cell[][] cells, int[] bombs){
+	private void generateCells(Cell[][] cells, int[] bombs, JFrame frame){
 		int cellID = 1;
 		for (int x = 0; x < 8; x++) {
 			for (int y = 0; y < 8; y++) {
 				boolean isBomb = checkCell(cellID);
 				if (isBomb) {
-					cells[x][y] = new Cell(cellID, true);
+					cells[x][y] = new Cell(cellID, true, frame);
 				} else {
-					cells[x][y] = new Cell(cellID, false);
+					cells[x][y] = new Cell(cellID, false, frame);
 				}
 				cellID += 1;
 			}
