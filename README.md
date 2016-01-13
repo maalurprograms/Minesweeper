b82e594be8cb1a2ab145cb5b25ed4ce6afedd953
maalurprograms <jonascosandey@hotmail.com>
2016-01-13 20:36:35 +0100

    Made new class diagram

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 A school project with Java and Swing.
 
 Fieldsize is 8x8 and there are 10 bombs.
+
+Classdigram tool: http://www.objectaid.com/class-diagram
--- src/Game.java
+++ src/Game.java
@@ -6,22 +6,24 @@ import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 
 public class Game {
+	
+	static final Field field = new Field();
+	static final Cell[][] cells = field.cells;
+	
 	public static void main(String[] args) {
-		Field field = new Field();
-		
-		for (int x = 0; x < field.cells.length; x++) {
-			for (int y = 0; y < field.cells.length; y++) {
-				setMouseListener(field.cells[x][y], field);
+		for (int x = 0; x < cells.length; x++) {
+			for (int y = 0; y < cells.length; y++) {
+				setMouseListener(cells[x][y]);
 			}
 		}		
 	}
 	
-	private static void setMouseListener(final Cell cell, final Field field) {
+	private static void setMouseListener(final Cell cell) {
 		cell.button.addMouseListener (new MouseAdapter(){
 		    public void mouseClicked(MouseEvent mouseEvent){
 		    	int modifiers = mouseEvent.getModifiers();
 		        if ((modifiers & InputEvent.BUTTON1_MASK) == InputEvent.BUTTON1_MASK) {
-		        	reveal(cell, field);
+		        	reveal(cell);
 		        }
 		        if ((modifiers & InputEvent.BUTTON3_MASK) == InputEvent.BUTTON3_MASK) {
 		        	mark(cell);
@@ -42,30 +44,28 @@ public class Game {
 		}
 	}
 	
-	private static void reveal(Cell cell, Field field) {
-		Cell[][] cells = field.cells;
+	private static void reveal(Cell cell) {
 		if (!cell.revealed) {
 			cell.revealed = true;
 			cell.button.setBackground(Color.WHITE);
 			if (!cell.bomb) {
 				if (cell.bombsInRange == 0) {
-					revealAllZero(field, cell);
+					revealAllZero(cell);
 				} else {
 					cell.button.setText(String.valueOf(cell.bombsInRange));
 				}
 				field.countRevealedCells += 1;
 				cell.setFontColor();
 				if (field.countRevealedCells == ((cells.length * cells.length) - field.bombs.length)) {
-					end(cells, true);
+					end(true);
 				}
 			} else {
-				end(cells, false);
+				end(false);
 			}
 		}
 	}
 	
-	private static void revealAllZero(Field field, Cell cell) {
-		Cell[][] cells = field.cells;
+	private static void revealAllZero(Cell cell) {
 		int[][] range = new int[][] {{-1, 1}, {0, 1},{1, 1},{-1, 0},{1, -1},{-1, -1},{0, -1},{1, 0}};
 		Cell cellInRange;
 		for (int x = 0; x < cells.length; x++) {
@@ -75,9 +75,9 @@ public class Game {
 						try {
 							cellInRange = cells[x + range[i][0]][y + range[i][1]];
 							if (cellInRange.bombsInRange == 0) {
-								reveal(cellInRange, field);
+								reveal(cellInRange);
 							} else if (cellInRange.bomb == false) {
-								reveal(cellInRange, field);
+								reveal(cellInRange);
 							}
 						} catch (Exception e) {
 
@@ -88,7 +88,7 @@ public class Game {
 		}
 	}
 	
-	private static void end(Cell[][] cells, boolean win) {
+	private static void end(boolean win) {
 		for (int x = 0; x < cells.length; x++) {
 			for (int y = 0; y < cells.length; y++) {
 				try {
